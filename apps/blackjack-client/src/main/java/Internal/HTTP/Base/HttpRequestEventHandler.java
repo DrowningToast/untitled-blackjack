efcbddf2af03d490143275ecb6cf1585bfb3e7b3
Suchanan239 <65070239@kmitl.ac.th>
2023-05-11 21:03:08 +0700

    HTTP

---
+++ apps/blackjack-client/src/main/java/Internal/HTTP/Base/HttpRequestEventHandler.java
@@ -0,0 +1,22 @@
+package Internal.HTTP.Base;
+
+import java.io.IOException;
+import java.net.URL;
+
+public interface HttpRequestEventHandler {
+
+    // MUST RETURN RESPONSE IN ORDER FOR onSuccess and onFail to process
+    HttpResponse executeRequest(URL baseURL) throws IOException;
+
+    void onSuccess(HttpResponse response);
+
+    /**
+     * Request error excetpion
+     * @param response
+     * @param e
+     */
+    void onFail(HttpResponse response, Exception e);
+
+
+
+}
