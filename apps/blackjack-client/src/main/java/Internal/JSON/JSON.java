692aa1e17822e2fcb8b42d9e04b6ee85713c3264
Suchanan239 <65070239@kmitl.ac.th>
2023-05-11 20:58:56 +0700

    JSON

--- apps/blackjack-client/src/main/java/Internal/JSON/JSON.java
+++ apps/blackjack-client/src/main/java/Internal/JSON/JSON.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 public class JSON {
     public static HashMap<String, String> parseJSONtoHashMap(String string) {
         Gson gson = new Gson();
-        Type type = new HashMap<String, Object>().getClass();
+        Type type = new HashMap<String, String>().getClass();
 
         HashMap<String, String> hashMap = gson.fromJson(string, type);
         return hashMap;
