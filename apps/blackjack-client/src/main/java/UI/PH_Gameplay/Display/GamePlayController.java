02b6643cbe31bd352b92f3aaf767a5e55d09e076
Wiches09 <wachnk09@gmail.con>
2023-05-15 00:32:41 +0700

    Minor change and delete unused file

--- apps/blackjack-client/src/main/java/UI/PH_Gameplay/Display/GamePlayController.java
+++ apps/blackjack-client/src/main/java/UI/PH_Gameplay/Display/GamePlayController.java
@@ -1,15 +1,8 @@
 package UI.PH_Gameplay.Display;
 
-
 import Internal.Websocket.Controller.WebsocketController;
 import UI.Controller.UIController;
-import jakarta.websocket.DeploymentException;
 import lombok.Getter;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.HashMap;
-
 
 public class GamePlayController {
     private WebsocketController wsController;
@@ -21,23 +14,6 @@ public class GamePlayController {
         this.wsController = wsController;
         this.uiController = uiController;
         ui = new GamePlayDisplayGUI(this, wsController);
-//        this.client = wsController;
-
     }
 
-//    public void actionHit() throws IOException {
-//        MessageBuilder message = new MessageBuilder(client);
-//        HashMap content = new HashMap();
-//        content.put("handler", "hit");
-//        message.setContent(content).send();
-//        System.out.println(content);
-//    }
-//
-//    public void actionStand() throws IOException {
-//        MessageBuilder message = new MessageBuilder(client);
-//        HashMap content = new HashMap();
-//        content.put("handler", "stand");
-//        message.setContent(content).send();
-//        System.out.println(content);
-//    }
 }
