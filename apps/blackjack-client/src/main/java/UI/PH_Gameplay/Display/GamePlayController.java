8a7f8675451dba7ad99af814869182729906d41e
Suchanan239 <65070239@kmitl.ac.th>
2023-05-15 02:48:15 +0700

    delete old code in showCard

--- apps/blackjack-client/src/main/java/UI/PH_Gameplay/Display/GamePlayController.java
+++ apps/blackjack-client/src/main/java/UI/PH_Gameplay/Display/GamePlayController.java
@@ -30,58 +30,22 @@ public class GamePlayController {
         this.ctx = ctx;
 
     }
-    public void showCardTwo(ArrayList<CardPOJO> cards){
+
+    public void showCardTwo(ArrayList<CardPOJO> cards) {
         ui.getPlayerTwoTable().removeAll();
-        for (CardPOJO i : cards){
+        for (CardPOJO i : cards) {
             cardPlayerTwo.showCard(i);
             ui.getPlayerTwoTable().add(cardPlayerTwo.getCard());
         }
     }
+
     public void showCardOne(ArrayList<CardPOJO> cards) {
         ui.getPlayerOneTable().removeAll();
-        for (CardPOJO i : cards){
+        for (CardPOJO i : cards) {
             cardPlayerOne.showCard(i);
             ui.getPlayerOneTable().add(cardPlayerOne.getCard());
         }
-//        cardPlayerOne.showCard(ctx.getPlayers()[0].getPlayer().getCardController().getCards());
-//        if ((ctx.getPlayers()[0].getPlayer().getCardController().getCards().size() == 2) && (ctx.getPlayers()[1].getPlayer().getCardController().getCards().size() == 2)) {
-//            cardPlayerOne.showCard(ctx.getPlayers()[0].getPlayer().getCardController().getCards().get(0));
-//            ui.getPlayerOneTable().add(cardPlayerOne.getCard());
-//            cardPlayerOne.showCard(ctx.getPlayers()[0].getPlayer().getCardController().getCards().get(1));
-//            ui.getPlayerOneTable().add(cardPlayerOne.getCard());
-//            cardPlayerTwo.showCard(ctx.getPlayers()[1].getPlayer().getCardController().getCards().get(0));
-//            ui.getPlayerTwoTable().add(cardPlayerTwo.getCard());
-//            cardPlayerTwo.showCard(ctx.getPlayers()[1].getPlayer().getCardController().getCards().get(1));
-//            ui.getPlayerTwoTable().add(cardPlayerTwo.getCard());
-//            playerPOJOOne.setScore(playerPOJOOne.getScore() + ctx.getPlayers()[0].getPlayer().getCardController().getCards().get(0).getValue());
-//            playerPOJOOne.setScore(playerPOJOOne.getScore() + ctx.getPlayers()[0].getPlayer().getCardController().getCards().get(1).getValue());
-//            playerPOJOTwo.setScore(playerPOJOTwo.getScore() + ctx.getPlayers()[1].getPlayer().getCardController().getCards().get(0).getValue());
-//            playerPOJOTwo.setScore(playerPOJOTwo.getScore() + ctx.getPlayers()[1].getPlayer().getCardController().getCards().get(1).getValue());
-//        }
-//        else {
-//        if (hit.equals("PlayerOne's hit")) {
-//            cardPlayerOne.showCard(ctx.getPlayers()[0].getPlayer().getCardController().getCards().get(ctx.getPlayers()[0].getPlayer().getCardController().getCards().size()-1));
-//            ui.getPlayerOneTable().add(cardPlayerOne.getCard());
-//            playerPOJOOne.setScore(playerPOJOOne.getScore() + ctx.getPlayers()[0].getPlayer().getCardController().getCards().get(ctx.getPlayers()[0].getPlayer().getCardController().getCards().size()-1).getValue());
-////            if (playerPOJOOne.getScore() >= 21) {
-////                gameGUI.getHitButtonPlayerOne().setVisible(false);
-////                gameGUI.getStandButtonPlayerOne().setVisible(false);
-////                playerPOJOOne.setStand(false);
-////            }
-//            ui.getScoreCardOneLabel().setText("Score: " + playerPOJOOne.getScore());
-////        } else if (hit.equals("PlayerTwo's hit")) {
-//            cardPlayerTwo.showCard(ctx.getPlayers()[1].getPlayer().getCardController().getCards().get(ctx.getPlayers()[1].getPlayer().getCardController().getCards().size()-1));
-//            ui.getPlayerTwoTable().add(cardPlayerTwo.getCard());
-//            playerPOJOTwo.setScore(playerPOJOTwo.getScore() + ctx.getPlayers()[1].getPlayer().getCardController().getCards().get(ctx.getPlayers()[1].getPlayer().getCardController().getCards().size()-1).getValue());
-////            if (playerPOJOTwo.getScore() >= 21) {
-////                gameGUI.getHitButtonPlayerTwo().setVisible(false);
-////                gameGUI.getStandButtonPlayerTwo().setVisible(false);
-////                playerPOJOOne.setStand(false);
-////            }
-//            ui.getScoreCardTwoLabel().setText("Score: " + playerPOJOTwo.getScore());
-        }
-//    }
-
+    }
 }
 
-//}
+
