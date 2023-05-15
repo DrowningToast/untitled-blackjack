b1f411cc822efe77f7f19bfdac1692305c8c79e1
Suchanan239 <65070239@kmitl.ac.th>
2023-05-15 18:16:23 +0700

    update tam comment

--- apps/blackjack-client/src/main/java/UI/Gameplay/GameplayController.java
+++ apps/blackjack-client/src/main/java/UI/Gameplay/GameplayController.java
@@ -15,18 +15,12 @@ import java.util.ArrayList;
 public class GameplayController {
     private WebsocketController wsController;
     private UIController uiController;
-    private CardController card;
     @Getter
     private GameplayDisplayGUI ui;
-//    public PlayerPOJO playerPOJOOne;
-//    public PlayerPOJO playerPOJOTwo;
     public CardDisplay cardPlayer;
 
     public GameplayController(UIController uiController, WebsocketController wsController) {
-//        playerPOJOOne = MainRunner.getGameContext().getPlayers()[0].getPOJO();
-//        playerPOJOTwo = MainRunner.getGameContext().getPlayers()[1].getPOJO();
         cardPlayer = new CardDisplay();
-//        cardPlayerTwo = new CardDisplay();
         this.wsController = wsController;
         this.uiController = uiController;
         ui = new GameplayDisplayGUI(this, wsController);
@@ -44,27 +38,17 @@ public class GameplayController {
         }
     }
 
-    public void showCard(JPanel playerTable,JLabel playerCardScore, PlayerPOJO player) {
-//        if (player.getUsername() == playerPOJOOne.getUsername()) {
-            playerTable.removeAll();
-            playerTable.revalidate();
-            playerTable.repaint();
-            for (CardPOJO i : player.getCardController().getPOJOS()) {
-                cardPlayer.showCard(i);
-                playerTable.add(cardPlayer.getCard());
-                playerCardScore.setText("Score : " + player.getCardScore());
-            }
-//        } else if (player.getUsername() == playerPOJOTwo.getUsername()) {
-//            playerTable.removeAll();
-//            playerTable.revalidate();
-//            playerTable.repaint();
-//            for (CardPOJO i : playerPOJOTwo.getCardController().getPOJOS()) {
-//                cardPlayerTwo.showCard(i);
-//                playerTable.add(cardPlayerTwo.getCard());
-//                ui.getScoreCardTwoLabel().setText("Score : " + player.getCardScore());
-//            }
+    public void showCard(JPanel playerTable, JLabel playerCardScore, PlayerPOJO player) {
+        playerTable.removeAll();
+        playerTable.revalidate();
+        playerTable.repaint();
+        for (CardPOJO i : player.getCardController().getPOJOS()) {
+            cardPlayer.showCard(i);
+            playerTable.add(cardPlayer.getCard());
+            playerCardScore.setText("Score : " + player.getCardScore());
         }
     }
-//}
+}
+
 
 
