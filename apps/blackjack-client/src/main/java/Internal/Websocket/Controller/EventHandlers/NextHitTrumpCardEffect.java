2d55d47cb588a6e0f7e0207d6ebea0c425d7f61d
Corust <siraopob.dar@gmail.com>
2023-05-16 18:31:48 +0700

    fix : NextHitTrumpCardEffect(see next 2 card) JOptionPane to display card's name

--- apps/blackjack-client/src/main/java/Internal/Websocket/Controller/EventHandlers/NextHitTrumpCardEffect.java
+++ apps/blackjack-client/src/main/java/Internal/Websocket/Controller/EventHandlers/NextHitTrumpCardEffect.java
@@ -7,7 +7,7 @@ import org.json.simple.JSONObject;
 
 import javax.swing.*;
 
-public class NextHitTrumpCardEffect implements WebsocketEventHandler{
+public class NextHitTrumpCardEffect implements WebsocketEventHandler {
     private UIController uiController;
 
     public NextHitTrumpCardEffect(UIController uiController) {
@@ -17,7 +17,11 @@ public class NextHitTrumpCardEffect implements WebsocketEventHandler{
     @Override
     public void handler(GameContext ctx, JSONObject body) {
         JSONArray cards = (JSONArray) body.get("content");
-        JOptionPane.showMessageDialog(null, "Next 2 cards from deck + \n 1. "+cards.get(0)+"\n 2. " + cards.get(1), "See the Future !", JOptionPane.INFORMATION_MESSAGE);
+        JSONObject card1 = (JSONObject) cards.get(0);
+        JSONObject card2 = (JSONObject) cards.get(1);
+        String cardShow1 = (String) card1.get("display");
+        String cardShow2 = (String) card2.get("display");
+        JOptionPane.showMessageDialog(null, "Next 2 cards from deck is \n 1. Card " + cardShow1 + "\n 2. Card " + cardShow2, "See the Future !", JOptionPane.INFORMATION_MESSAGE);
         ctx.getLogController().addLog(ctx.getPlayers()[0].getPOJO().getUsername() + " is watching the future!?");
     }
 }
