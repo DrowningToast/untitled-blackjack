2cf292ccb83831c7de4ef4577c0b30a2c5362fe0
Wiches09 <wachnk09@gmail.con>
2023-05-19 19:01:36 +0700

    add JOptionPane to tell that other player is disconnected

--- apps/blackjack-client/src/main/java/Internal/Websocket/Controller/EventHandlers/GameStopDueQuitting.java
+++ apps/blackjack-client/src/main/java/Internal/Websocket/Controller/EventHandlers/GameStopDueQuitting.java
@@ -4,6 +4,8 @@ import GameContext.GameContext;
 import Internal.UserInterface.UIController;
 import org.json.simple.JSONObject;
 
+import javax.swing.*;
+
 public class GameStopDueQuitting implements WebsocketEventHandler{
     private UIController uiController;
 
@@ -15,6 +17,8 @@ public class GameStopDueQuitting implements WebsocketEventHandler{
     public void handler(GameContext ctx, JSONObject body) {
         String username = (String) body.get("content");
         ctx.getLogController().addLog("Player " + username + " has left the game, The game is now safe to leave.");
+        JOptionPane.showMessageDialog(null,"Your opponent has quit the game, You win!");
         uiController.update();
+        System.exit(0);
     }
 }
